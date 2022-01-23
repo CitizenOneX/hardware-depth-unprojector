33ec14721d45ae192cb0a8041c8a67264278f41d
CitizenOne <17427385+CitizenOneX@users.noreply.github.com>
2022-01-23 22:25:31 +1100

    added back hdu files after snafu. Example file not present though.

---
+++ hdu.c
@@ -0,0 +1,94 @@
+/*
+ * HDU - Hardware Depth Unprojector C library implementation
+ *
+ * Copyright 2020 (C) Bartosz Meglicki <meglickib@gmail.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#include "hdu.h"
+
+#include <stdio.h> //fprintf
+#include <stdlib.h> //malloc
+
+//in binary 10 ones followed by 6 zeroes
+static const uint16_t P010LE_MAX = 0xFFC0;
+
+struct hdu
+{
+	float ppx;
+	float ppy;
+	float fx;
+	float fy;
+	float depth_unit;
+	float min_depth;
+	float max_depth;
+};
+
+struct hdu *hdu_init(const struct hdu_config *c)
+{
+	struct hdu *h;
+
+	if( ( h = (struct hdu*)malloc(sizeof(struct hdu))) == NULL )
+	{
+		fprintf(stderr, "hdu: not enough memory for hdu\n");
+		//errno = ENOMEM;
+		return NULL;
+	}
+
+	h->ppx = c->ppx;
+	h->ppy = c->ppy;
+	h->fx = c->fx;
+	h->fy = c->fy;
+	h->depth_unit = c->depth_unit;
+	h->min_depth = c->min_margin;
+	h->max_depth = P010LE_MAX * c->depth_unit - c->max_margin;
+
+	return h;
+}
+
+void hdu_close(struct hdu *h)
+{
+	if(h == NULL)
+		return;
+
+	free(h);
+}
+
+void hdu_unproject(const struct hdu *h, const struct hdu_depth *depth, struct hdu_point_cloud *pc)
+{
+	const int pc_size = pc->size;
+	const uint8_t default_color = 0xFF; // 0xFFFFFFFF for 32-bit
+	int points=0;
+	float d;
+
+	for(int r=0;r<depth->height;++r)
+		for(int c=0;c<depth->width && points < pc_size;++c)
+		{
+			d = depth->data[r * depth->depth_stride / 2 + c] * h->depth_unit;
+
+			//if (d <= h->min_depth || d > h->max_depth)
+			//	continue;
+
+			pc->data[points][0] = d * (c - h->ppx) / h->fx;
+			pc->data[points][1] = -d * (r - h->ppy) / h->fy;
+			pc->data[points][2] = d;
+
+			if (depth->colors)
+			{
+				const uint8_t* color_line = (((uint8_t*)depth->colors) + r * depth->color_stride);
+				pc->colors[points] = color_line[c];
+			}
+			else
+			{
+				pc->colors[points] = default_color;
+			}
+			++points;
+		}
+
+	pc->used = points;
+	return;
+}
